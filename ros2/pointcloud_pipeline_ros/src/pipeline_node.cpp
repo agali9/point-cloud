da6969a3c312de5dd36814dd9465296fcea675a8
agali9 <agali9@asu.edu>
2026-05-11 23:12:00 -0700

    stub ros2 pipeline node

---
+++ ros2/pointcloud_pipeline_ros/src/pipeline_node.cpp
@@ -0,0 +1,22 @@
+﻿#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "pointcloud_pipeline/pipeline.hpp"
+
+class PipelineNode : public rclcpp::Node {
+public:
+    PipelineNode() : Node("pointcloud_pipeline_node") {
+        RCLCPP_INFO(get_logger(), "point cloud pipeline node started");
+    }
+
+private:
+    pointcloud_pipeline::PointCloudPipeline pipeline_;
+};
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PipelineNode>());
+    rclcpp::shutdown();
+    return 0;
+}
